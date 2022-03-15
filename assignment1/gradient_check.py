bef4a66eea555c212d23dff5b09ba9e185cfeb22
Konstantin Berengolts <dc.kokis@gmail.com>
2022-03-15 16:37:38 +0700

    Add files via upload

--- assignment1/gradient_check.py
+++ assignment1/gradient_check.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 
-def check_gradient(f, x, delta=1e-5, tol = 1e-4):
-    '''
+def check_gradient(f, x, delta=1e-5, tol=1e-4):
+    """
     Checks the implementation of analytical gradient by comparing
     it to numerical gradient using two-point formula
 
@@ -14,11 +14,11 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
 
     Return:
       bool indicating whether gradients match or not
-    '''
-    
+    """
+
     assert isinstance(x, np.ndarray)
     assert x.dtype == np.float
-    
+
     orig_x = x.copy()
     fx, analytic_grad = f(x)
     assert np.all(np.isclose(orig_x, x, tol)), "Functions shouldn't modify input variables"
@@ -32,18 +32,17 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
     while not it.finished:
         ix = it.multi_index
         analytic_grad_at_ix = analytic_grad[ix]
-        numeric_grad_at_ix = 0
-
-        # TODO compute value of numeric gradient of f to idx
+        x[ix] += delta
+        x1 = f(x)[0]
+        x[ix] -= 2 * delta
+        x2 = f(x)[0]
+        numeric_grad_at_ix = (x1 - x2) / (2 * delta)
         if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
-            print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (ix, analytic_grad_at_ix, numeric_grad_at_ix))
+            print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (
+                ix, analytic_grad_at_ix, numeric_grad_at_ix))
             return False
 
         it.iternext()
 
     print("Gradient check passed!")
     return True
-
-        
-
-        
--- assignment1/metrics.py
+++ assignment1/metrics.py
@@ -1,5 +1,5 @@
 def binary_classification_metrics(prediction, ground_truth):
-    '''
+    """
     Computes metrics for binary classification
 
     Arguments:
@@ -8,22 +8,33 @@ def binary_classification_metrics(prediction, ground_truth):
 
     Returns:
     precision, recall, f1, accuracy - classification metrics
-    '''
-    precision = 0
-    recall = 0
-    accuracy = 0
-    f1 = 0
+    """
 
-    # TODO: implement metrics!
     # Some helpful links:
     # https://en.wikipedia.org/wiki/Precision_and_recall
     # https://en.wikipedia.org/wiki/F1_score
-    
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
+    for i in range(len(prediction)):
+        if prediction[i] and ground_truth[i]:
+            tp += 1
+        if prediction[i] and not ground_truth[i]:
+            fp += 1
+        if not prediction[i] and ground_truth[i]:
+            fn += 1
+        if not prediction[i] and not ground_truth[i]:
+            tn += 1
+    accuracy = (tp + tn) / (tp + tn + fp + fn)
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f1 = 2 * (precision * recall) / (precision + recall)
     return precision, recall, f1, accuracy
 
 
 def multiclass_accuracy(prediction, ground_truth):
-    '''
+    """
     Computes metrics for multiclass classification
 
     Arguments:
@@ -32,6 +43,9 @@ def multiclass_accuracy(prediction, ground_truth):
 
     Returns:
     accuracy - ratio of accurate predictions to total samples
-    '''
-    # TODO: Implement computing accuracy
-    return 0
+    """
+    tp = 0
+    for i in range(len(prediction)):
+        if prediction[i] == ground_truth[i]:
+            tp += 1
+    return tp / len(prediction)
