34f1a314e1fdd73a572dc50d301ecc0c8cdf626c
Konstantin Berengolts <63435062+dckokis@users.noreply.github.com>
2022-04-03 17:38:07 +0700

    assignment 3 done

--- assignment3/gradient_check.py
+++ assignment3/gradient_check.py
@@ -27,11 +27,14 @@ def check_gradient(f, x, delta=1e-5, tol=1e-4):
     while not it.finished:
         ix = it.multi_index
         analytic_grad_at_ix = analytic_grad[ix]
-        numeric_grad_at_ix = 0
+        coord = x.copy()
+        coord[ix] += delta
+        fx1 = f(coord)[0]
 
-        # TODO Copy from previous assignment
-        raise Exception("Not implemented!")
+        coord[ix] -= 2 * delta
+        fx2 = f(coord)[0]
 
+        numeric_grad_at_ix = (fx1 - fx2) / (2 * delta)
         if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
             print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (
                   ix, analytic_grad_at_ix, numeric_grad_at_ix))
--- assignment3/model.py
+++ assignment3/model.py
@@ -3,8 +3,8 @@ import numpy as np
 from layers import (
     FullyConnectedLayer, ReLULayer,
     ConvolutionalLayer, MaxPoolingLayer, Flattener,
-    softmax_with_cross_entropy, l2_regularization
-    )
+    softmax_with_cross_entropy
+)
 
 
 class ConvNet:
@@ -15,6 +15,7 @@ class ConvNet:
     Conv[3x3] -> Relu -> MaxPool[4x4] ->
     Flatten -> FC -> Softmax
     """
+
     def __init__(self, input_shape, n_output_classes, conv1_channels, conv2_channels):
         """
         Initializes the neural network
@@ -26,8 +27,22 @@ class ConvNet:
         conv1_channels, int - number of filters in the 1st conv layer
         conv2_channels, int - number of filters in the 2nd conv layer
         """
-        # TODO Create necessary layers
-        raise Exception("Not implemented!")
+        width, height, channels = input_shape
+        filter_size = 3
+        padding = 1
+        pool_size = 4
+        pool_stride = 4
+        left_width = width // pool_stride // pool_stride
+        left_height = height // pool_stride // pool_stride
+        self.layers = [ConvolutionalLayer(channels, conv1_channels, filter_size, padding),
+                       ReLULayer(),
+                       MaxPoolingLayer(pool_size, pool_stride),
+                       ConvolutionalLayer(conv1_channels, conv2_channels, filter_size, padding),
+                       ReLULayer(),
+                       MaxPoolingLayer(pool_size, pool_stride),
+                       Flattener(),
+                       FullyConnectedLayer(left_width * left_height * conv2_channels, n_output_classes)
+                       ]
 
     def compute_loss_and_gradients(self, X, y):
         """
@@ -40,21 +55,32 @@ class ConvNet:
         """
         # Before running forward and backward pass through the model,
         # clear parameter gradients aggregated from the previous pass
+        for _, v in self.params().items():
+            v.grad.fill(0.0)
+        forward_out = X
+        for layer in self.layers:
+            forward_out = layer.forward(forward_out)
 
-        # TODO Compute loss and fill param gradients
-        # Don't worry about implementing L2 regularization, we will not
-        # need it in this assignment
-        raise Exception("Not implemented!")
+        loss, d_out = softmax_with_cross_entropy(forward_out, y)
+        backward_out = d_out
+        for layer in reversed(self.layers):
+            backward_out = layer.backward(backward_out)
+        return loss
 
     def predict(self, X):
-        # You can probably copy the code from previous assignment
-        raise Exception("Not implemented!")
+        forward_out = X
+        for layer in self.layers:
+            forward_out = layer.forward(forward_out)
+        return np.argmax(forward_out, axis=1)
 
     def params(self):
         result = {}
+        name2layer = {"Conv1": self.layers[0],
+                      "Conv2": self.layers[3],
+                      "Fully": self.layers[7]}
 
-        # TODO: Aggregate all the params from all the layers
-        # which have parameters
-        raise Exception("Not implemented!")
+        for name, layer in name2layer.items():
+            for k, v in layer.params().items():
+                result['{}_{}'.format(name, k)] = v
 
         return result
